23b0917f86ffdb0a3fc74801b7ca1622d2107b74
Stanley Mubanga <estbannabum@gmail.com>
2025-06-14 09:16:03 +0200

    update

--- src/api/applicant/controllers/applicant.ts
+++ src/api/applicant/controllers/applicant.ts
@@ -5,8 +5,49 @@
 // import { factories } from '@strapi/strapi'
 
 // export default factories.createCoreController('api::applicant.applicant');
+// import { factories } from '@strapi/strapi';
+// import { sanitize } from '@strapi/utils';
+
+// export default factories.createCoreController('api::applicant.applicant', ({ strapi }) => ({
+//     async search(ctx) {
+//         const { email, phone, name } = ctx.query;
+
+//         const orFilters = [];
+
+//         if (email) {
+//         orFilters.push({ email: { $containsi: email } });
+//         }
+//         if (phone) {
+//         orFilters.push({ phone: { $containsi: phone } });
+//         }
+//         if (name) {
+//         orFilters.push({ name: { $containsi: name } });
+//         }
+
+//         if (orFilters.length === 0) {
+//         // If no search params provided, return empty result
+//         return [];
+//         }
+
+//         // const results = await strapi.entityService.findMany('api::applicant.applicant', {
+//         // filters: {
+//         //     $or: orFilters,
+//         // },
+//         // });
+
+//         const results = await strapi.entityService.findMany('api::applicant.applicant', {
+//         filters: { $or: orFilters },
+//         populate: '*',
+//         bypassPermission: true,
+// });
+
+//         const sanitizedResults = await sanitize.contentAPI.output(results, strapi.getModel('api::applicant.applicant'));
+
+//         return sanitizedResults;
+//     },
+// }));
+
 import { factories } from '@strapi/strapi';
-import { sanitize } from '@strapi/utils';
 
 export default factories.createCoreController('api::applicant.applicant', ({ strapi }) => ({
     async search(ctx) {
@@ -25,18 +66,15 @@ export default factories.createCoreController('api::applicant.applicant', ({ str
         }
 
         if (orFilters.length === 0) {
-        // If no search params provided, return empty result
         return [];
         }
 
         const results = await strapi.entityService.findMany('api::applicant.applicant', {
-        filters: {
-            $or: orFilters,
-        },
+        filters: { $or: orFilters },
+        populate: '*',
+        bypassPermission: true,
         });
 
-        const sanitizedResults = await sanitize.contentAPI.output(results, strapi.getModel('api::applicant.applicant'));
-
-        return sanitizedResults;
+        return results;
     },
 }));
