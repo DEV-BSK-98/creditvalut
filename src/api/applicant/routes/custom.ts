379eb93e51d103f3e9ebdb59ba6ca2d5933657f1
Stanley Mubanga <estbannabum@gmail.com>
2025-06-14 09:33:14 +0200

    update

--- src/api/applicant/routes/custom.ts
+++ src/api/applicant/routes/custom.ts
@@ -1,12 +1,11 @@
-import { Route } from '@strapi/strapi';
-
 export default [
-    Route({
+    {
         method: 'GET',
         path: '/applicants/search',
         handler: 'api::applicant.applicant.search',
         config: {
-        auth: false,
+        auth: false, // optional: allow public access
+        policies: [],
         },
-    }),
+    },
 ];
